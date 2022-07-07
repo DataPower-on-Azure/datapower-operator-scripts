87f4c510dc676f8f31cced619ff8e3f6ae2690d1
pbennettibm <Patrick.Bennett@ibm.com>
2022-07-07 15:28:31 -0500

    change string validation

--- migrate-backup-service.sh
+++ migrate-backup-service.sh
@@ -6,7 +6,7 @@ PORTS=$@
 
 PORTLIST=$(
   for PORT in {$PORTS}; do
-    if [[ $PORT  != $NAME ]]; then
+    if [[ "$PORT"  != "$NAME" ]]; then
       echo "    - name: $NAME-$PORT";
       echo "      protocol: TCP";
       echo "      port: $PORT";
