d2bd62814c844b7989d64cf76e5200de5723dc75
pbennettibm <Patrick.Bennett@ibm.com>
2022-07-07 16:25:56 -0500

    change validation

--- migrate-backup-route.sh
+++ migrate-backup-route.sh
@@ -6,7 +6,7 @@ PORTS=$@
 
 PORTLIST=$(
   for PORT in {$PORTS}; do
-    if [[ $PORT  =~ ^[0-9]+$ ]]; then
+    if [[ "$PORT" = "$NAME" ]]; then
       echo "    targetPort: $NAME-$PORT";
     else
       echo $PORT
